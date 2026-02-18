4dcf786e1f03dd3551dce4069cecfa1257c066e4
Richard <dev.basic0588@gmail.com>
2026-02-18 11:17:41 -0500

    fixed build error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Exclude old src directory from build
+  webpack: (config) => {
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/src/**', '**/node_modules/**'],
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
